f1bf855fbdeb4abf7aab29dc6525dececa1e5758
전강현 <aniren@debian.moe>
2023-08-30 11:11:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from lets_common_log import logUtils as log
 from flask import Flask, render_template, session, redirect, url_for, request, send_from_directory, jsonify, send_file, Response, jsonify
 from functions import *
 import os
+import config
+
+conf = config.config("config.ini")
 
 app = Flask(__name__)
 
@@ -29,17 +32,27 @@ def audio(id):
     return id
     #return send_file(read_audio(id), mimetype='audio/mpeg')
 
+@app.route('/video/<id>')
+def video(id):
+    readed_read_video = read_video(id)
+    if readed_read_video.endswith(".mp4"):
+        return send_file(readed_read_video, mimetype='video/mp4')
+    else:
+        return {"code": 404, "message": "Sorry Beatmap has no videos", "funcmsg": readed_read_video}
+
 def stream_osz(id):
-    response = send_file(read_osz(id), mimetype='application/x-osu-beatmap-archive')
-    response.headers["Content-Disposition"] = f"attachment; filename={read_osz(id).replace('dl/', '')}"
+    path = read_osz(id)
+    response = send_file(path['path'], mimetype='application/x-osu-beatmap-archive')
+    response.headers["Content-Disposition"] = f"attachment; filename={path['filename']}"
     return response
 @app.route('/d/<id>')
 def osz(id):
     return stream_osz(id)
 
 def stream_osu(id):
-    response = send_file(read_osu(id), mimetype='application/x-osu-beatmap')
-    response.headers["Content-Disposition"] = f"attachment; filename={read_osu(id).replace('osu/', '')}"
+    path = read_osu(id)
+    response = send_file(path['path'], mimetype='application/x-osu-beatmap')
+    response.headers["Content-Disposition"] = f"attachment; filename={path['filename']}"
     return response
 @app.route('/osu/<id>')
 def osu(id):
@@ -48,5 +61,9 @@ def osu(id):
 
 if __name__ == "__main__":
     folder_check()
-    app.run(host= '0.0.0.0', port=6200, threaded= False, debug=True)
+    if conf.config["server"]["flaskdebug"] == "0":
+        debugMode = False
+    else:
+        debugMode = True
+    app.run(host= '0.0.0.0', port=conf.config["server"]["port"], threaded= False, debug=debugMode)
 
