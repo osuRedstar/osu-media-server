11909f17ee2612782b204401a88a31c4a81a98e6
전강현 <aniren@debian.moe>
2023-09-01 13:58:20 +0900

    web

--- static/js/index.js
+++ static/js/index.js
@@ -1,52 +1,153 @@
 const bg_tag = document.getElementById("bg");
-const thumb = document.getElementById("thumb")
-const bancho_userid = document.getElementById("banchouserid");
-const bancho_username = document.getElementById("banchousername");
+const thumb_tag = document.getElementById("thumb")
+const audio_tag = document.getElementById("audio");
+const preview_tag = document.getElementById("preview");
+const video_tag = document.getElementById("video");
+const b_tag = document.getElementById("b");
+const d_tag = document.getElementById("d");
+const osu_tag = document.getElementById("osu");
 
 function bg(arg) {
     //arg.preventDefault();
     const get_id = prompt("+BeatmapSetID or BeatmapID")
-    if (get_id === null) {
+    if (get_id === null | get_id === "") {
         alert("정확하게 입력하세요! \nPlease enter it correctly!");
         get_id = "";
         location.reload(true);
     }
     /* else if (isNaN(get_id) === true) {
-        alert("당신의 ID(숫자)를 입력하세요! \nEnter your ID (number)!")
+        alert("숫자를 입력하세요! \nEnter Number!")
         location.reload(true);
     } */
-    bg_tag.href = `https://media.redstar.moe/bg/${get_id}`
-
+    else {
+        bg_tag.href = `${document.location.href}bg/${get_id}`
+    }
 }
 
-function BanchoUserId(arg) {
+function thumb(arg) {
     //arg.preventDefault();
-    const get_id = prompt("당신의 Bancho ID를 적어주세요! \nPlease write down your Bancho ID!")
-    if (get_id === null) {
+    const get_id = prompt("BeatmapSetID")
+    if (get_id === null | get_id === "") {
         alert("정확하게 입력하세요! \nPlease enter it correctly!");
         get_id = "";
         location.reload(true);
     }
     else if (isNaN(get_id) === true) {
-        alert("당신의 ID(숫자)를 입력하세요! \nEnter your ID (number)!")
+        alert("숫자를 입력하세요! \nEnter Number!")
         location.reload(true);
     }
-    bancho_userid.href = `https://a.redstar.moe/bancho/id/${get_id}`
-
+    else {
+        thumb_tag.href = `${document.location.href}thumb/${get_id}l.jpg`
+    }
 }
 
-function BanchoUserName(arg) {
+function audio(arg) {
     //arg.preventDefault();
-    const get_id = prompt("당신의 Bancho Name를 적어주세요! \nPlease write down your Bancho Name!")
-    if (get_id === null) {
+    const get_id = prompt("+BeatmapSetID or BeatmapID")
+    if (get_id === null | get_id === "") {
         alert("정확하게 입력하세요! \nPlease enter it correctly!");
         get_id = "";
         location.reload(true);
     }
-    bancho_username.href = `https://a.redstar.moe/bancho/u/${get_id}`
+    /* else if (isNaN(get_id) === true) {
+        alert("숫자를 입력하세요! \nEnter Number!")
+        location.reload(true);
+    } */
+    else {
+        audio_tag.href = `${document.location.href}audio/${get_id}`
+    }
+}
 
+function preview(arg) {
+    //arg.preventDefault();
+    const get_id = prompt("BeatmapSetID")
+    if (get_id === null | get_id === "") {
+        alert("정확하게 입력하세요! \nPlease enter it correctly!");
+        get_id = "";
+        location.reload(true);
+    }
+    else if (isNaN(get_id) === true) {
+        alert("숫자를 입력하세요! \nEnter Number!")
+        location.reload(true);
+    }
+    else {
+        preview_tag.href = `${document.location.href}preview/${get_id}.mp3`
+    }
+}
+
+function video(arg) {
+    //arg.preventDefault();
+    const get_id = prompt("BeatmapID")
+    if (get_id === null | get_id === "") {
+        alert("정확하게 입력하세요! \nPlease enter it correctly!");
+        get_id = "";
+        location.reload(true);
+    }
+    else if (isNaN(get_id) === true) {
+        alert("숫자를 입력하세요! \nEnter Number!")
+        location.reload(true);
+    }
+    else {
+        video_tag.href = `${document.location.href}video/${get_id}`
+    }
+}
+
+function b(arg) {
+    //arg.preventDefault();
+    const get_id = prompt("BeatmapID")
+    if (get_id === null | get_id === "") {
+        alert("정확하게 입력하세요! \nPlease enter it correctly!");
+        get_id = "";
+        location.reload(true);
+    }
+    else if (isNaN(get_id) === true) {
+        alert("숫자를 입력하세요! \nEnter Number!")
+        location.reload(true);
+    }
+    else {
+        b_tag.href = `${document.location.href}b/${get_id}`
+    }
+}
+
+function d(arg) {
+    //arg.preventDefault();
+    const get_id = prompt("BeatmapSetID")
+    if (get_id === null | get_id === "") {
+        alert("정확하게 입력하세요! \nPlease enter it correctly!");
+        get_id = "";
+        location.reload(true);
+    }
+    else if (isNaN(get_id) === true) {
+        alert("숫자를 입력하세요! \nEnter Number!")
+        location.reload(true);
+    }
+    else {
+        d_tag.href = `${document.location.href}d/${get_id}`
+    }
+}
+
+function osu(arg) {
+    //arg.preventDefault();
+    const get_id = prompt("BeatmapID")
+    if (get_id === null | get_id === "") {
+        alert("정확하게 입력하세요! \nPlease enter it correctly!");
+        get_id = "";
+        location.reload(true);
+    }
+    else if (isNaN(get_id) === true) {
+        alert("숫자를 입력하세요! \nEnter Number!")
+        location.reload(true);
+    }
+    else {
+        osu_tag.href = `${document.location.href}osu/${get_id}`
+    }
 }
 
 bg_tag.addEventListener("click", bg);
-bancho_userid.addEventListener("click", BanchoUserId);
-bancho_username.addEventListener("click", BanchoUserName);
+thumb_tag.addEventListener("click", thumb);
+audio_tag.addEventListener("click", audio);
+preview_tag.addEventListener("click", preview);
+video_tag.addEventListener("click", video);
+b_tag.addEventListener("click", b);
+d_tag.addEventListener("click", d);
+osu_tag.addEventListener("click", osu);
