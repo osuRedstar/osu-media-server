f5e77a1ffbbfa9243bf63e78522d32a0f3f542e5
전강현 <aniren@debian.moe>
2023-09-05 16:15:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,43 +10,57 @@ conf = config.config("config.ini")
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         self.render("templates/index.html")
 
 class ListHandler(tornado.web.RequestHandler):
     def get(self):
         # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         self.write(read_list())
 
 class BgHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         self.set_header('Content-Type', 'image/jpeg')
         with open(read_bg(id), 'rb') as f:
             self.write(f.read())
@@ -55,15 +69,20 @@ class BgHandler(tornado.web.RequestHandler):
 class ThumbHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         self.set_header('Content-Type', 'image/jpeg')
         with open(read_thumb(id), 'rb') as f:
             self.write(f.read())
@@ -71,15 +90,20 @@ class ThumbHandler(tornado.web.RequestHandler):
 class PreviewHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         self.set_header('Content-Type', 'audio/mpeg')
         with open(read_audio(id), 'rb') as f:
             self.write(f.read())
@@ -87,15 +111,20 @@ class PreviewHandler(tornado.web.RequestHandler):
 class AudioHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         self.set_header('Content-Type', 'audio/mpeg')
         with open(read_preview(id), 'rb') as f:
             self.write(f.read())
@@ -103,15 +132,20 @@ class AudioHandler(tornado.web.RequestHandler):
 class VideoHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         readed_read_video = read_video(id)
         if readed_read_video.endswith(".mp4"):
             self.set_header('Content-Type', 'video/mp4')
@@ -123,15 +157,20 @@ class VideoHandler(tornado.web.RequestHandler):
 class OszHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         path = read_osz(id)
         if path == 0:
             self.write("ERROR")
@@ -144,15 +183,20 @@ class OszHandler(tornado.web.RequestHandler):
 class OszBHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         path = read_osz_b(id)
         if path == 0:
             self.write("ERROR")
@@ -165,15 +209,20 @@ class OszBHandler(tornado.web.RequestHandler):
 class OsuHandler(tornado.web.RequestHandler):
     def get(self, id):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         path = read_osu(id)
         self.set_header('Content-Type', 'application/x-osu-beatmap')
         self.set_header('Content-Disposition', f'attachment; filename={path["filename"]}')
@@ -183,15 +232,20 @@ class OsuHandler(tornado.web.RequestHandler):
 class FaviconHandler(tornado.web.RequestHandler):
     def get(self):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         self.set_header('Content-Type', 'image/png')
         with open("static/img/favicon.png", 'rb') as f:
             self.write(f.read())
@@ -199,15 +253,20 @@ class FaviconHandler(tornado.web.RequestHandler):
 class StaticHandler(tornado.web.RequestHandler):
     def get(self, item):
         # Logging the request IP address
+        # Logging the request IP address
+        print("")
         try:
             real_ip = self.request.headers["Cf-Connecting-Ip"]
             request_uri = self.request.headers["X-Forwarded-Proto"] + "://" + self.request.host + self.request.uri
+            country_code = self.request.headers["Cf-Ipcountry"]
         except:
-            log.warning("cloudflare를 거치지 않아서 real_ip 조회가 안댐")
+            log.warning("cloudflare를 거치지 않아서 real_ip, country_code 조회가 안댐")
             real_ip = "0.0.0.0"
             request_uri = self.request.protocol + "://" + self.request.host + self.request.uri
+            country_code = "XX"
         client_ip = self.request.remote_ip
-        log.chat(f"Request from IP: {real_ip}, {client_ip} | URL: {request_uri}")
+        User_Agent = self.request.headers["User-Agent"]
+        log.info(f"Request from IP: {real_ip}, {client_ip} ({country_code}) | URL: {request_uri} | From: {User_Agent}")
         with open(f"static/{item}", 'rb') as f:
                 self.write(f.read())
 
