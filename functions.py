212aa5e3730f6044633f7ae7ccc41bd0267f04c2
전강현 <aniren@debian.moe>
2025-05-28 00:49:13 +0900

    readableMods & readableModsReverse

--- functions.py
+++ functions.py
@@ -383,8 +383,11 @@ def getAcc(play_mode: int, count_300: int, count_100: int, count_50: int, gekis_
 	else: accuracy = 0 #?
 	return accuracy
 
-def readableMods(__mods: int):
+def readableMods(__mods: int) -> str:
     """
+    Return a string with readable std mods.
+    Used to convert a mods number for oppai
+
     :param __mods: mods bitwise number
     :return: readable mods string, eg HDDT
     """
@@ -392,7 +395,7 @@ def readableMods(__mods: int):
     if __mods == mods.NOMOD: return ""
     if __mods & mods.NOFAIL: r += "NF"
     if __mods & mods.EASY: r += "EZ"
-    if __mods & mods.TOUCHSCREEN: r += "TD"
+    if __mods & mods.TOUCHSCREEN: r += "TD" #(NV)
     if __mods & mods.HIDDEN: r += "HD"
     if __mods & mods.HARDROCK: r += "HR"
     if __mods & mods.SUDDENDEATH: r += "SD"
@@ -401,7 +404,7 @@ def readableMods(__mods: int):
     if __mods & mods.HALFTIME: r += "HT"
     if __mods & mods.NIGHTCORE: r = r.replace("DT", "NC") if "DT" in r else r + "NC" #576 = DT, NC
     if __mods & mods.FLASHLIGHT: r += "FL"
-    if __mods & mods.AUTOPLAY: r += "AU(AUTO)"
+    if __mods & mods.AUTOPLAY: r += "AT"
     if __mods & mods.SPUNOUT: r += "SO"
     if __mods & mods.RELAX2: r += "AP"
     if __mods & mods.PERFECT: r = r.replace("SD", "PF") if "SD" in r else r + "PF" #16416 = SD, PF
@@ -410,27 +413,24 @@ def readableMods(__mods: int):
     if __mods & mods.KEY6: r += "K6"
     if __mods & mods.KEY7: r += "K7"
     if __mods & mods.KEY8: r += "K8"
-    if __mods & mods.KEYMOD: r += "KEYMOD" #?
+    #if __mods & mods.KEYMOD: r += "KEYMOD" #?
     if __mods & mods.FADEIN: r += "FI"
     if __mods & mods.RANDOM: r += "RD"
-    if __mods & mods.LASTMOD: r += "LASTMOD" #?
+    #if __mods & mods.LASTMOD: r += "LASTMOD" #?
     if __mods & mods.KEY9: r += "K9"
-    if __mods & mods.KEY10: r += "K10"
+    if __mods & mods.COOP: r += "CP"
     if __mods & mods.KEY1: r += "K1"
     if __mods & mods.KEY3: r += "K3"
     if __mods & mods.KEY2: r += "K2"
-    if __mods & mods.SCOREV2: r += "SV2(v2)"
+    if __mods & mods.SCOREV2: r += "V2" #(SV2)
     if __mods & mods.MIRROR: r += "MR"
     return r
 
-def readableModsReverse(__mods: str):
-    # Check passed mods and convert to enum
-    if len(__mods.upper().replace("K10", "")) % 2: __mods = "^^"
-    if "K10" in __mods: __mods = __mods.replace("K10", "") + "K10"
+def readableModsReverse(__mods: str) -> int:
     modsEnum = 0
-    for r in [__mods[i:i+3].upper() if __mods[i:i+3].upper() == "K10" else __mods[i:i+2].upper() for i in range(0, len(__mods) if not "K10" in __mods else len(__mods) - 1, 2)]:
-        if r not in ["NO", "NF", "EZ", "TD", "HD", "HR", "SD", "DT", "HT", "NC", "FL", "SO", "PF", "RX", "AP", "K4", "K5", "K6", "K7", "K8", "FI", "RD", "K9", "K10", "K1", "K3", "K2", "v2", "MR"]:
-            return "Invalid mods. Allowed mods: NO, NF, EZ, TD, HD, HR, SD, DT, HT, NC, FL, SO, PF, RX, AP, K4, K5, K6, K7, K8, FI, RD, K9, K10, K1, K3, K2, v2(SV2), MR. Do not use spaces for multiple mods."
+    for r in [__mods[i:i+2].upper() for i in range(0, len(__mods), 2)]:
+        if r not in ["NO", "NF", "EZ", "TD", "HD", "HR", "SD", "DT", "HT", "NC", "FL", "SO", "PF", "RX", "AP", "K4", "K5", "K6", "K7", "K8", "FI", "RD", "K9", "CP", "K1", "K3", "K2", "V2", "MR"]:
+            return "Invalid mods. Allowed mods: NO, NF, EZ, TD(NV), HD, HR, SD, DT, HT, NC, FL, SO, PF, RX, AP, K4, K5, K6, K7, K8, FI, RD, K9, CP, K1, K3, K2, V2(SV2), MR. Do not use spaces for multiple mods."
         if r == "NO": return 0
         elif r == "NF": modsEnum += mods.NOFAIL
         elif r == "EZ": modsEnum += mods.EASY
@@ -441,12 +441,12 @@ def readableModsReverse(__mods: str):
         elif r == "DT": modsEnum += mods.DOUBLETIME
         elif r == "RX": modsEnum += mods.RELAX
         elif r == "HT": modsEnum += mods.HALFTIME
-        elif r == "NC": modsEnum += 576 #576 = DT, NC
+        elif r == "NC": modsEnum += mods.NIGHTCORE + mods.DOUBLETIME #576 #576 = DT, NC
         elif r == "FL": modsEnum += mods.FLASHLIGHT
         elif r == "AT": modsEnum += mods.AUTOPLAY
         elif r == "SO": modsEnum += mods.SPUNOUT
         elif r == "AP": modsEnum += mods.RELAX2
-        elif r == "PF": modsEnum += 16416 #16416 = SD, PF
+        elif r == "PF": modsEnum += mods.PERFECT + mods.SUDDENDEATH #16416 #16416 = SD, PF
         elif r == "K4": modsEnum += mods.KEY4
         elif r == "K5": modsEnum += mods.KEY5
         elif r == "K6": modsEnum += mods.KEY6
@@ -457,11 +457,11 @@ def readableModsReverse(__mods: str):
         elif r == "RD": modsEnum += mods.RANDOM
         #elif r == "LASTMOD": modsEnum += mods.LASTMOD
         elif r == "K9": modsEnum += mods.KEY9
-        elif r == "K10": modsEnum += mods.KEY10
+        elif r == "CP": modsEnum += mods.COOP
         elif r == "K1": modsEnum += mods.KEY1
         elif r == "K3": modsEnum += mods.KEY3
         elif r == "K2": modsEnum += mods.KEY2
-        elif r == "v2": modsEnum += mods.SCOREV2
+        elif r == "V2": modsEnum += mods.SCOREV2
         elif r == "MR": modsEnum += mods.MIRROR
     return modsEnum
 
