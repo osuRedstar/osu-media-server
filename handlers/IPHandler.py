ce33646abd15d403a5e086f4e388e94baaf2463a
전강현 <aniren@debian.moe>
2025-10-07 23:29:00 +0900

    사소한 인자 추가

--- handlers/IPHandler.py
+++ handlers/IPHandler.py
@@ -8,11 +8,13 @@ from helpers import requestsManager
 
 class handler(requestsManager.asyncRequestHandler):
     def asyncGet(self, requestIP):
+        try: isjson = int(self.get_argument("isjson", 1))
+        except: isjson = 1
         rm = request_msg(self, botpass=True)
         if rm != 200: pass
         data = IPtoFullData(requestIP)
         log.info(f"{data['ip']} ({data['country']}) | IP 조회 들어옴")
         self.set_header("return-fileinfo", json.dumps({"filename": "", "path": "", "fileMd5": ""}))
-        self.set_header("Content-Type", pathToContentType(".json")["Content-Type"])
+        if isjson: self.set_header("Content-Type", pathToContentType(".json")["Content-Type"])
         self.write(json.dumps(data, indent=2, ensure_ascii=False))
         self.set_header("Ping", str(resPingMs(self)))
--- handlers/StatusHandler.py
+++ handlers/StatusHandler.py
@@ -8,14 +8,18 @@ from helpers import requestsManager
 
 class handler(requestsManager.asyncRequestHandler):
     def asyncGet(self):
+        try:
+            isjson = int(self.get_argument("isjson", 1))
+            isinfo = int(self.get_argument("isinfo", 1))
+        except: isjson = isinfo = 1
         rm = request_msg(self, botpass=True)
         if rm != 200: pass
         real_ip, request_url, country_code, client_ip, User_Agent, Referer, IsCloudflare, IsNginx, IsHttp, Server = getRequestInfo(self)
         data = {
             "code": 200,
-            "oszCount": read_list()["osz"]["count"],
-            "oszSize": get_dir_size(f"{dataFolder}/dl"),
-            "filesSize": get_dir_size(f"{dataFolder}/Songs"),
+            "oszCount": read_list()["osz"]["count"] if isinfo else -1,
+            "oszSize": get_dir_size(f"{dataFolder}/dl") if isinfo else -1,
+            "filesSize": get_dir_size(f"{dataFolder}/Songs") if isinfo else -1,
             "requestTime": round(time.time()),
             "request": {
                 "IP": real_ip,
@@ -32,6 +36,6 @@ class handler(requestsManager.asyncRequestHandler):
             }
         }
         self.set_header("return-fileinfo", json.dumps({"filename": "", "path": "", "fileMd5": ""}))
-        self.set_header("Content-Type", pathToContentType(".json")["Content-Type"])
+        if isjson: self.set_header("Content-Type", pathToContentType(".json")["Content-Type"])
         self.write(json.dumps(data, indent=2, ensure_ascii=False))
         self.set_header("Ping", str(resPingMs(self)))
